4b5dbb5fdef189d63867a9e52a645b2ae762e893
Paul Rostorp <paul.rostorp@gmail.com>
2022-01-19 16:18:38 +0100

    init

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "no-unreachable": ["error"],
+    quotes: ["error", "double"],
+    semi: ["error", "always"],
+    "prettier/prettier": [
+      "error",
+      {
+        printWidth: 100,
+        semi: true,
+        bracketSpacing: true,
+        arrowParens: "avoid",
+      },
+    ],
+    "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "@typescript-eslint/explicit-function-return-type": ["error"],
+  },
+};
